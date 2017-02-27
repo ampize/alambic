d1aac2c28397b6d60a74a24b487919e051acf325
Alexandru Dobre <alexandru.dobre@webtales.fr>
2017-02-27 14:54:56 +0100

    fix connector

--- src/Alambic/Connector/AbstractConnector.php
+++ src/Alambic/Connector/AbstractConnector.php
@@ -19,26 +19,26 @@ abstract class AbstractConnector
     protected $limit = 10;
     protected $orderBy = null;
     protected $orderByDirection = 'DESC';
-    protected $argsDefinition;
+    protected $argsDefinition=[];
     protected $requiredConfig = [];
     protected $requiredArgs = [];
     protected $connection;
 
     abstract public function __invoke($payload=[]);
 
-    protected function setPayload($payload) {
-         $this->payload = $payload;
-         $configs = isset($payload["configs"]) ? $payload["configs"] : [];
-         $baseConfig=isset($payload["connectorBaseConfig"]) ? $payload["connectorBaseConfig"] : [];
-         $this->config = array_merge($baseConfig, $configs);
-         $this->args=isset($this->payload["args"]) ? $payload["args"] : [];
-         $this->multivalued=isset($payload["multivalued"]) ? $payload["multivalued"] : false;
-         if (!empty($payload['pipelineParams']['start'])) $this->start = $payload['pipelineParams']['start'];
-         if (!empty($payload['pipelineParams']['limit'])) $this->limit = $payload['pipelineParams']['limit'];
-         if (!empty($payload['pipelineParams']['orderBy'])) $this->orderBy = $payload['pipelineParams']['orderBy'];
-         if (!empty($payload['pipelineParams']['orderByDirection'])) $this->orderByDirection = $payload['pipelineParams']['orderByDirection'];
-         if (!empty($payload['pipelineParams']['argsDefinition'])) $this->argsDefinition = $payload['pipelineParams']['argsDefinition'];
-     }
+    protected function setPayload($payload){
+        $this->payload = $payload;
+        $configs = isset($payload["configs"]) ? $payload["configs"] : [];
+        $baseConfig=isset($payload["connectorBaseConfig"]) ? $payload["connectorBaseConfig"] : [];
+        $this->config = array_merge($baseConfig, $configs);
+        $this->args=isset($this->payload["args"]) ? $payload["args"] : [];
+        $this->multivalued=isset($payload["multivalued"]) ? $payload["multivalued"] : false;
+        $this->start =!empty($payload['pipelineParams']['start']) ? $payload['pipelineParams']['start'] : 0;
+        $this->limit =!empty($payload['pipelineParams']['limit']) ? $payload['pipelineParams']['limit'] : 10;
+        $this->orderBy =!empty($payload['pipelineParams']['orderBy']) ? $payload['pipelineParams']['orderBy'] : null;
+        $this->orderByDirection =!empty($payload['pipelineParams']['orderByDirection']) ? $payload['pipelineParams']['orderByDirection'] : 'DESC';
+        $this->argsDefinition =!empty($payload['pipelineParams']['$argsDefinition']) ? $payload['pipelineParams']['$argsDefinition'] : [];
+    }
 
     protected function checkConfig() {
         foreach($this->requiredConfig as $var => $msg) {
