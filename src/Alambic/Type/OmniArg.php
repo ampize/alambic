6ca0531363f4ee5cbb1af36e62f7ad5483aa0c6c
Alexandru Dobre <alexandru.dobre@webtales.fr>
2017-04-20 16:11:26 +0200

    WIP filters

---
+++ src/Alambic/Type/OmniArg.php
@@ -0,0 +1,44 @@
+<?php
+namespace Alambic\Type;
+
+use GraphQL\Language\AST\BooleanValueNode;
+use GraphQL\Language\AST\FloatValueNode;
+use GraphQL\Language\AST\StringValueNode;
+use GraphQL\Language\AST\IntValueNode;
+use GraphQL\Type\Definition\ScalarType;
+
+class OmniArg  extends ScalarType
+{
+
+    public $name = 'OmniArg';
+
+    public function serialize($value)
+    {
+        return $this->parseValue($value);
+    }
+
+    public function parseValue($value)
+    {
+        if ($value === true) {
+            return 'true';
+        }
+        if ($value === false) {
+            return 'false';
+        }
+        return (string) $value;
+    }
+
+    public function parseLiteral($ast)
+    {
+        if($ast instanceof IntValueNode){
+            return (int) $ast->value;
+        } elseif ($ast instanceof FloatValueNode){
+            return (float) $ast->value;
+        } elseif ($ast instanceof BooleanValueNode){
+            return (bool) $ast->value;
+        } elseif ($ast instanceof StringValueNode){
+            return (bool) $ast->value;
+        }
+        return null;
+    }
+}
